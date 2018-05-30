c8f7473584f6103769c09c1ad6492e090e895491
Blaise Schaeffer <Blaise.Schaeffer1@ibm.com>
2018-05-30 16:48:56 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { RestService } from './services/rest.service';
+import { GraphComponent } from './d3/visuals/graph/graph.component';
+import { NodeVisualComponent } from './d3/visuals/node-visual/node-visual.component';
+import { LinkVisualComponent } from './d3/visuals/link-visual/link-visual.component';
+import { ZoomableDirective } from './d3/directives/zoomable.directive';
+import { DraggableNodeDirective } from './d3/directives/draggable-node.directive';
+import { FamilyVisualizationComponent } from './family-visualization/family-visualization.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FamilyVisualizationComponent,
+    GraphComponent,
+    NodeVisualComponent,
+    LinkVisualComponent,
+    ZoomableDirective,
+    DraggableNodeDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    RestService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
